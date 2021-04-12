38fecd28b254b17c2955ae7069f09a20811d1abc
moripark32 <moripark32@gmail.com>
2021-04-12 17:19:31 +0900

    이름폴더 수정/router.js수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Login from "./Pages/sp/Login/login";
-import SignUp from './Pages/sp/SignUp/signUp';
-import ProductDetail from "./Pages/se/ProductDetail/productDetail";
-import Main from "./Pages/mj/Main/Main";
+import Login from "./Pages/Login/login";
+import SignUp from './Pages/SignUp/signUp';
+import ProductDetail from "./Pages/ProductDetail/productDetail";
+import Main from "./Pages/Main/Main";
 
 export class Routes extends Component {
     render() {
         return (
             <Router>
                 <Switch>
-                    <route>
-                        <Route exact path="/product-detail" component={ProductDetail} />
-                        <Route exact path="/login" component={Login} />
-                        <Route exact path="/sign-up" component={SignUp} />
-                        <Route exact path="/main" component={Main} />
-                    </route>
+                    <Route exact path="/product-detail" component={ProductDetail} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/sign-up" component={SignUp} />
+                    <Route exact path="/main" component={Main} />
                 </Switch>
             </Router>
         )
     }
 }
-
 export default Routes
