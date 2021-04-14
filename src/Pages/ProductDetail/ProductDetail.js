3822573f68c8eeabcabd2dea00b5c2ee7a033df8
SungeunPark <elenability@naver.com>
2021-04-14 17:41:36 +0900

    Fix: product detail fix

--- src/Pages/ProductDetail/ProductDetail.js
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
-import NavBar from '../../Components/NavBar/NavBar';
 
 export class ProductDetail extends Component {
   render() {
     return (
       <div>
-        {/* top banner */}
-        <div className="top_banner">
-          <a href="#">지금 그리팅 가입하고 최대 10만원 쿠폰팩 받으세요 > </a>
-        </div>
-        {/* navbar */}
-        <NavBar />
         {/* product detail */}
         <main className="product_detail">
           <div className="location">
