0225aaa01fa63e47bf06eb02a767570f63d927a6
moripark32 <moripark32@gmail.com>
2021-04-21 13:55:56 +0900

    login ready to be merged

--- src/Routes.js
+++ src/Routes.js
@@ -2,21 +2,13 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Login from './Pages/Login/Login';
-// import SignUp from './Pages/SignUp/SignUp';
-// import SignUpDetail from './Pages/SignUpDetail/SignUpDetail';
-// import ProductDetail from './Pages/ProductDetail/ProductDetail';
-import Main from './Pages/Main/Main';
 
 export class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          {/* <Route exact path="/product-detail" component={ProductDetail} /> */}
           <Route exact path="/login" component={Login} />
-          {/* <Route exact path="/sign-up" component={SignUp} /> */}
-          {/* <Route exact path="/sign-up-detail" component={SignUpDetail} /> */}
-          <Route exact path="/main" component={Main} />
         </Switch>
       </Router>
     );
