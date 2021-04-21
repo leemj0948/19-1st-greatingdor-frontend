898c1159bdd4be73f763762af90d297796a483a1
moripark32 <moripark32@gmail.com>
2021-04-21 13:43:16 +0900

    sign-up merge신청

--- src/Pages/Login/login.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class login extends Component {
-  render() {
-    return <div>hello login</div>;
-  }
-}
-
-export default login;
--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignUp from './Pages/SignUp/SignUp';
-import ProductDetail from './Pages/ProductDetail/ProductDetail';
-import Cart from './Pages/Cart/Cart';
 import SignUpDetail from './Pages/SignUpDetail/SignUpDetail';
 
 export class Routes extends Component {
@@ -10,10 +8,8 @@ export class Routes extends Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/product-detail" component={ProductDetail} />
           <Route exact path="/sign-up" component={SignUp} />
           <Route exact path="/sign-up-detail" component={SignUpDetail} />
-          <Route exact path="/cart" component={Cart} />
         </Switch>
       </Router>
     );
