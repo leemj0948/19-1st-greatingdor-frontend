3fce0c125de8a41e3b842bf22a3bbf53fb7b3b0c
moripark32 <moripark32@gmail.com>
2021-04-22 18:21:15 +0900

    cart modified

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -8,9 +8,6 @@ class Cart extends Component {
     };
   }
   handleIncrease = () => {
-    // if (this.state.number > 30) {
-    //   return 30;
-    // }
     this.setState({
       number: this.state.number + 1,
     });
@@ -28,86 +25,80 @@ class Cart extends Component {
   };
   render() {
     return (
-      <main className="cart-wrapper">
+      <main className="cartWrapper">
         <h2> 장바구니 </h2>
-        <div className="cart-content">
+        <div className="cartContent">
           <div className="selectAllOrDelete">
             <button>전체선택</button>
             <button>선택삭제</button>
           </div>
-          <div className="select-products">
-            <form className="check-wrapper"></form>
-            <div className="cart-select">
-              <ul className="cart-select-list">
-                <li className="item">
-                  <label>
-                    <input className="check-circle" type="checkbox" />
-                  </label>
-                  <img scr="/" alt="no img" />
-                  <div className="item-name">
-                    <a href="#" className="package">
-                      [달래된장]] 여기 맵
-                    </a>
-                    <a href="#" className="product">
-                      [달래된장] 대애앤장{' '}
-                    </a>
-                  </div>
-
-                  <form className="count-btn" onSubmit={this.handleSubmit}>
-                    <button>
-                      <i class="fas fa-minus" onClick={this.handleDecrease}></i>
-                    </button>
-                    <input type="text" value={this.state.number}></input>
-                    <button onClick={this.handleIncrease}>
-                      <i class="fas fa-plus"></i>
-                    </button>
-                  </form>
-                  <div className="price">
-                    <dl className="total-price-wrapper">
-                      <dt className="total-price">
-                        백엔드에서 받는 가격<span>원</span>
-                      </dt>
-                    </dl>
-                    <button className="delete-btn">
-                      <i class="fas fa-times"></i>
-                    </button>
-                  </div>
-                </li>
-              </ul>
-            </div>
+          <div className="selectProducts">
+            <ul className="cartSelectList">
+              <li className="item">
+                <label>
+                  <input className="checkCircle" type="checkbox" />
+                </label>
+                <img scr="/" alt="no img" />
+                <div className="itemName">
+                  <a href="#" className="package">
+                    [달래된장]] 여기 맵
+                  </a>
+                  <a href="#" className="product">
+                    [달래된장] 대애앤장{' '}
+                  </a>
+                </div>
+                <form className="countBtn" onSubmit={this.handleSubmit}>
+                  <button>
+                    <i class="fas fa-minus" onClick={this.handleDecrease}></i>
+                  </button>
+                  <input type="text" value={this.state.number}></input>
+                  <button onClick={this.handleIncrease}>
+                    <i class="fas fa-plus"></i>
+                  </button>
+                </form>
+                <div className="price">
+                  <dl className="totalPriceWrapper">
+                    <dt className="totalPrice">
+                      백엔드에서 받는 가격<span>원</span>
+                    </dt>
+                  </dl>
+                  <button className="deleteBtn">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </li>
+            </ul>
           </div>
-          <div className="cart-result">
-            <div className="inner-result">
-              <div className="amount-view">
-                <dl className="dl">
-                  <dt className="dt">상품금액</dt>
-                  <dd>
-                    <span className="total-price">
-                      0<span>원</span>
-                    </span>
-                  </dd>
-                </dl>
-                <dl className="dl">
-                  <dt className="dt">상품할인금액</dt>
-                  <dd>
-                    <span>
-                      0<span>원</span>
-                    </span>
-                  </dd>
-                </dl>
-                <dl className="final-dl">
-                  <dt className="dt">결제예정금액</dt>
-                  <dd>
-                    <span className="final-price">
-                      8800<span>원</span>
-                    </span>
-                  </dd>
-                </dl>
-              </div>
-              <form className="submit">
-                <button className="submit-btn">구매하기</button>
-              </form>
+          <div className="cartResult">
+            <div className="priceView">
+              <dl className="dl">
+                <dt className="dt">상품금액</dt>
+                <dd>
+                  <span className="totalPrice">
+                    0<span>원</span>
+                  </span>
+                </dd>
+              </dl>
+              <dl className="dl">
+                <dt className="dt">상품할인금액</dt>
+                <dd>
+                  <span>
+                    0<span>원</span>
+                  </span>
+                </dd>
+              </dl>
+              <dl className="finalDl">
+                <dt className="dt">결제예정금액</dt>
+                <dd>
+                  <span className="finalPrice">
+                    8800<span>원</span>
+                  </span>
+                </dd>
+              </dl>
             </div>
+            <form className="submit">
+              <button className="submitBtn">구매하기</button>
+            </form>
           </div>
         </div>
       </main>
