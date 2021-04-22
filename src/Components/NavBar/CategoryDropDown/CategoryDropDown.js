e8de8a2371972aa8ee1f31f9a5b6691106ed7373
leemj0948 <ljo094822@naver.com>
2021-04-22 21:28:15 +0900

    Restore

---
+++ src/Components/NavBar/CategoryDropDown/CategoryDropDown.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './CategoryDropDown.scss';
+
+class CategoryDropDown extends Component {
+  state = {
+    menuItemList: [],
+  };
+
+  componentDidMount = () => {
+    fetch('/data/MenuItems.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ menuItemList: data.RESULT });
+      });
+  };
+
+  onClickHandler = () => {
+    this.props.handleDropDown();
+  };
+
+  render() {
+    const { isClicked } = this.props;
+    const { menuItemList } = this.state;
+
+    return (
+      <div className="category_dropdown">
+        <ul
+          onClick={this.onClickHandler}
+          className={isClicked ? 'dropdown-menu clicked' : 'dropdown-menu'}
+        >
+          {menuItemList &&
+            menuItemList.map((item, idx) => {
+              return (
+                <li key={idx}>
+                  <div className="title">{item.menu}</div>
+                  {item.category.map((category, idx) => {
+                    return (
+                      <Link className="items" key={idx}>
+                        {category}
+                      </Link>
+                    );
+                  })}
+                </li>
+              );
+            })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default CategoryDropDown;
